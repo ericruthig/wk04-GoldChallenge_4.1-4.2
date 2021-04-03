96aee084cfd92981710e303a63aa7896ff9363f0
unknown <iam@ericruthig.com>
2021-04-03 07:37:09 -0500

    new

--- 4.1-classes.instructional.js
+++ 4.1-classes.instructional.js
@@ -1,7 +1,8 @@
 /*
     Create a class named Polygon that contains the following information:
 
-    - A constructor method with a parameter to capture information (hint: this should be capturing an array of numbers).
+    - A constructor method with a parameter to capture information (hint: this should be capturing an array of 
+        numbers).
         - assign the parameter to a key using 'this'
     - Has a method named perimeter, that should do the following:
         - return ONLY the parameter, or an empty array from the method
@@ -18,6 +19,25 @@
 
 // Create a polygon with side lengths 3, 3, 8, and 8
 
+var side1 = 3;
+var side2 = 3;
+var side3 = 8;
+var side4 = 8;
 
+let sides = [side1, side2, side3, side4]
+
+Polygon = class {
+    constructor(sides) {
+        this.sides.side1 = side1,
+        this.sides.side2 = side2,
+        this.sides.side3 = side3,
+        this.sides.side4 = side4;
+        // reducer = (sides );       
+        
+        const perimeter =  sides.reduce(total, amount) 
+        console.log(perimeter);
+        return perimeter
+    };
+};
   
 // Print the perimeter
--- 4.2 spread-destructuring.instructional.js
+++ 4.2 spread-destructuring.instructional.js
@@ -1,15 +1,30 @@
 /*
-    - With the given shoppingList array, use array destructuring to assign each array within the shoppingList array its own variable. Then, reinitialize the shoppingList array so it is a single array containing all of the values from the original array. 
+    - With the given shoppingList array, use array destructuring to assign each array within the shoppingList array
+     its own variable. Then, reinitialize the shoppingList array so it is a single array containing all of the 
+     values from the original array. 
     
-    - after the shoppingList array has been reinitialized, using the array.prototype.map method, map over the newly initialized shoppingList array. You should do the following within the map method:
+    - after the shoppingList array has been reinitialized, using the array.prototype.map method, map over the newly 
+    initialized shoppingList array. You should do the following within the map method:
         - include a parameter for index
-        - utilize the array.prototype.push method to push the current value being mapped over into the appropriate key/value pair in the obj variable
+        - utilize the array.prototype.push method to push the current value being mapped over into the appropriate 
+        key/value pair in the obj variable
             - you will need to implement some sort of logic to check the index of the current item being mapped over.
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
 
-let shoppingList = [[ 'eggs', 'milk', 'butter' ], [ 'cleaner', 'trash bags', 'detergent' ], [ 'thank you card', 'pens', 'gift wrapping' ], [ 'shoes', 't-shirt', 'slacks' ]];
+let shoppingList = [[ 'eggs', 'milk', 'butter' ], [ 'cleaner', 'trash bags', 'detergent' ], 
+[ 'thank you card', 'pens', 'gift wrapping' ], [ 'shoes', 't-shirt', 'slacks' ]];
+
+const [grocery, cleaning, gifts, clothing] = shoppingList;
+console.log('First ', grocery)
+
+shoppingList = [...grocery, ...cleaning, ...gifts, ...clothing];
+console.log('Grocery: ', grocery);
+console.log('Cleaning: ', cleaning);
+console.log('Gifts: ', gifts);
+console.log('Clothing: ', clothing);
+console.log('Shopping List:', shoppingList)
 
 let obj = {
 cleaning: [],
@@ -17,3 +32,27 @@ gifts: [],
 clothing: [],
 grocery: []
 };
+
+const arr = shoppingList;
+const newList = arr.map((x, i) => {
+    console.log(`value`,x, `index`, i);
+    if (i >= 0 && x <= 2) {
+        Array.prototype.push(grocery, obj.grocery);
+    } else if (i >= 3 && x <= 5) {
+        Array.prototype.push(cleaning, obj.cleaning);
+    } else if (i >= 6 && x <= 8) {
+        Array.prototype.push(gifts, obj.gifts);
+     } else {
+        Array.prototype.push(clothing, obj.clothing);
+    }
+    console.log(`Clothing:`, clothing); 
+    console.log(`Gifts:`, gifts);
+    console.log( `Cleaning:`, cleaning);
+    console.log( `Grocery:`, grocery);
+});
+
+// console.log(`Clothing:`, obj.clothing); 
+// console.log(`Gifts:`, obj.gifts);
+// console.log( `Cleaning:`, obj.cleaning);
+// console.log( `Grocery:`, obj.grocery);
+
